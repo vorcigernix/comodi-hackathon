09ece399ea6cda5387a55cbdfdb844b67514a9a1
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-01 18:45:26 +0100

    next updates

--- pages/_app.js
+++ pages/_app.js
@@ -8,22 +8,22 @@ function Marketplace({ Component, pageProps }) {
         <p className="text-4xl font-bold">Metaverse Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-4 text-amber-500">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-amber-500">
               Sell Digital Asset
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-amber-500">
               My Digital Assets
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-amber-500">
               Creator Dashboard
             </a>
           </Link>
