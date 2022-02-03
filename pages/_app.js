f8972ffd1ff43e76ad56d8671cc7d054d7bf45c3
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-03 12:42:44 +0100

    detect wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,40 @@
 import "../styles/globals.css";
 import Link from "next/link";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
 
 function Marketplace({ Component, pageProps }) {
+  const [provider, setProvider] = useState(null);
+  useEffect(() => {
+    if (window.ethereum) {
+      setProvider(new ethers.providers.Web3Provider(window.ethereum));
+    }
+    console.log(provider);
+  }, [provider]);
   return (
     <div>
       <nav className="border-b p-6">
         <Link href="/" passHref>
-        <a className="text-4xl text-sky-700 font-extrabold font-title">Comodi</a>
+          <a className="text-4xl text-sky-700 font-extrabold font-title">
+            Comodi
+          </a>
         </Link>
-        <div className="flex mt-4">
-          <Link href="/market">
-            <a className="mr-4 text-sky-500">Market</a>
-          </Link>
-          <Link href="/create-item">
-            <a className="mr-6 text-sky-500">Sell Digital Asset</a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-6 text-sky-500">My Digital Assets</a>
-          </Link>
-          <Link href="/creator-dashboard">
-            <a className="mr-6 text-sky-500">Creator Dashboard</a>
-          </Link>
-        </div>
+        {provider && (
+          <div className="flex mt-4">
+            <Link href="/market">
+              <a className="mr-4 text-sky-500">Market</a>
+            </Link>
+            <Link href="/create-item">
+              <a className="mr-6 text-sky-500">Sell Digital Asset</a>
+            </Link>
+            <Link href="/my-assets">
+              <a className="mr-6 text-sky-500">My Digital Assets</a>
+            </Link>
+            <Link href="/creator-dashboard">
+              <a className="mr-6 text-sky-500">Creator Dashboard</a>
+            </Link>
+          </div>
+        )}
       </nav>
       <Component {...pageProps} />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  useEffect(() => {
+    if (window.ethereum) {
+      setProvider(new ethers.providers.Web3Provider(window.ethereum));
+    }
+    console.log(provider);
+  }, [provider]);
   return (
     <>
       <section className="text-gray-600 body-font" name="hero">
@@ -9,16 +18,43 @@ export default function Home() {
               <br className="hidden lg:inline-block" />
               for this century
             </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
+            {provider ? (
+              <p className="mb-8 leading-relaxed">
+                Coolio bro, you have the wallet. Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+            ) : (
+              <p className="mb-8 leading-relaxed">
+                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+                park mlkshk tote bag selvage hot chicken authentic tumeric
+                truffaut hexagon try-hard chambray.
+              </p>
+            )}
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600 rounded text-lg">
-                Install Wallet
-              </button>
+              {provider ? (
+                <span className="inline-flex text-sky-500 bg-white-500 border-0 py-2 px-6  text-lg">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    className="text-sky-500 w-6 h-6 flex-shrink-0 mr-4"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  Wallet Installed
+                </span>
+              ) : (
+                <button className="inline-flex text-white bg-sky-500 border-0 py-2 px-6 focus:outline-none hover:bg-sky-600 rounded text-lg">
+                  Install Metamask
+                </button>
+              )}
               <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
                 Open Marketplace
               </button>
