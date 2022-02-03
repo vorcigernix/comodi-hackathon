84c7b40fc7d0c9c71986172cbdaa39a569a37f5f
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-03 17:19:21 +0100

    change contract name

--- contracts/CNft.sol
+++ contracts/CNft.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Nft is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract CNft is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
