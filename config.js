17f46f643a249069f3d9c3135da1e476907ad5ef
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-03 17:28:23 +0100

    new contract

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x4EB14051663e5921bE47F30dd69e2EA8925F767a"
-  export const nftaddress = "0xa4F1F02aB215e27b72bCB152A319D2844aC31F55"
+  export const nftmarketaddress = "0xcdcb3B5E1B9113fD66ED95A9AFaD8c83403E9B3C"
+  export const nftaddress = "0xC2a9E4ED41aDb176F28a1621710F2DA6c4E2b1e3"
   
