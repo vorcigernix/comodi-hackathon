f8677c9f7ac8a070c53396597f5fd5afc7904469
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-05 13:28:48 +0100

    creating works

--- pages/create-item.js
+++ pages/create-item.js
@@ -18,6 +18,8 @@ export default function CreateItem() {
     price: "",
     name: "",
     description: "",
+    sku: "",
+    qty: "",
   });
   const router = useRouter();
   useEffect(() => {
@@ -37,7 +39,8 @@ export default function CreateItem() {
   }, []);
 
   async function createMarket() {
-    const { name, description, price } = formInput;
+    const { name, description, price, sku, qty } = formInput;
+    console.log(name, description, price);
     if (!name || !description || !price || !fileUrl) return;
     /* first, upload to IPFS */
     const data = JSON.stringify({
@@ -90,12 +93,14 @@ export default function CreateItem() {
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            {fileUrl && (
+            {fileUrl ? (
               <img
                 className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
                 src={fileUrl}
                 alt="QR Code"
               />
+            ) : (
+              <div className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center bg-sky-200 rounded"></div>
             )}
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
