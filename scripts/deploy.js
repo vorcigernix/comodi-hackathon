aaebc8fd0e0328ab81cbb26a6d782066f289319c
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-02-05 23:33:13 +0100

    fixing project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,25 +2,21 @@ const hre = require("hardhat");
 const fs = require('fs');
 
 async function main() {
-  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const NFTMarket = await hre.ethers.getContractFactory("Market");
   const nftMarket = await NFTMarket.deploy();
   await nftMarket.deployed();
   console.log("nftMarket deployed to:", nftMarket.address);
 
-  const NFT = await hre.ethers.getContractFactory("NFT");
+  const NFT = await hre.ethers.getContractFactory("Nft");
   const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
 
-  const CNFT = await hre.ethers.getContractFactory("CNft");
-  const cnft = await CNFT.deploy(nftMarket.address);
-  await cnft.deployed();
-  console.log("cnft deployed to:", cnft.address);
+
 
   let config = `
   export const nftmarketaddress = "${nftMarket.address}"
   export const nftaddress = "${nft.address}"
-  export const cnftaddress = "${cnft.address}"
   `
 
   let data = JSON.stringify(config)
